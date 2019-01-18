c78f908dfc3adc062fbf5edc696ebf46b291d7a3
Patrick Tone <pt@patrickt.one>
2019-01-17 21:11:26 -0600

    Added documentation and simple tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
 
+  @doc """
+    Returns a list of tuples representing a deck of playing cards
+  """
   def create_deck do
     values = 1..13
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -10,18 +16,52 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Returns a deck with the cards shuffled
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Checks if the card exists within the deck
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> Cards.contains?(deck, {1, "Spades"})
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      [{1, "Spades"}]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Store the deck on the local file system
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> Cards.save(deck, "deck-o-cards")
+      :ok
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
 
@@ -36,6 +76,9 @@ defmodule Cards do
     "That file does not exist"
   end
 
+  @doc """
+    Load a deck from the local file system
+  """
   def load(filename) do
     filename |>
       File.read() |>
@@ -48,6 +91,15 @@ defmodule Cards do
     #end
   end
 
+  @doc """
+    Create a deck, shuffle the deck, and return a hand and the remainder of the deck
+
+  ## Examples
+
+      iex> {hand, deck} = Cards.create_hand(1)
+      iex> Enum.count(hand)
+      1
+  """
   def create_hand(hand_size) do
     Cards.create_deck() |>
       Cards.shuffle() |>
@@ -70,12 +122,14 @@ defmodule Cards do
     Cards.to_string({"King", suit})
   end
 
-  def to_string(deck) when is_list(deck) do
-    for card <- deck do
-      Cards.to_string(card)
-    end
-  end
+  @doc """
+    Convert the card to a string
 
+  ## Examples
+
+      iex> Cards.to_string({1, "Spades"})
+      "Ace of Spades"
+  """
   def to_string({value, suit}) do
     "#{value} of #{suit}"
   end
